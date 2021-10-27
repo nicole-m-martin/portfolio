495c21e4df2179cf3519773c6010bcdf3df38b12
Nicole Martin <n.martinpdx@gmail.com>
2021-10-26 20:20:08 -0700

    add styling

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -6,13 +6,15 @@ function Projects() {
 
 
 <div>
-{projects.map(({src, name, stack, description}) => (
+{projects.map(({src, name, stack, description, github, deployed}) => (
     
 <section>
 <img src={src} alt=''/>
-<h1>{name}</h1>
-<p>{stack}</p>
-<p>{description}</p> 
+<h1 className="uppercase tracking-wide text-sm text-indigo-600 font-bold">{name}</h1>
+<p className='tracking-wide text-sm text-green-600 font-bold'>{stack}</p>
+<p className="">{description}</p> 
+<a href={github} className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Github</a>
+          <a href={deployed} className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Deployed Site</a>
 </section>
     ))}
     
