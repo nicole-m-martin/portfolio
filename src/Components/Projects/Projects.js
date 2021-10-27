07f6951b49cc4a5fabdfdfd96e8da34a638ff51e
Nicole Martin <n.martinpdx@gmail.com>
2021-10-27 11:04:31 -0700

    deconstructed projects.map

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -10,11 +10,9 @@ function Projects() {
     
 <section>
 <img src={src} alt=''/>
-<h1 className="uppercase tracking-wide text-sm text-indigo-600 font-bold">{name}</h1>
-<p className='tracking-wide text-sm text-green-600 font-bold'>{stack}</p>
-<p className="">{description}</p> 
-<a href={github} className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Github</a>
-          <a href={deployed} className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Deployed Site</a>
+<h1>{name}</h1>
+<p>{stack}</p>
+<p>{description}</p> 
 </section>
     ))}
     
