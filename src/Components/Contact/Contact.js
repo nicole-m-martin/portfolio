94ecba6b9f81691cd751dafd352791b6a880ab55
Nicole Martin <n.martinpdx@gmail.com>
2021-10-27 20:50:36 -0700

    contact form created

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,38 +1,71 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function Contact() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  // Message countdown
+  const message = watch('message') || '';
+  const messageCharsLeft = 2500 - message.length;
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
-      CONTACT
-      <form id="contact-form">
+      <h1>Contact Me!</h1>
+      <form id="contact-form" onSubmit={handleSubmit(onSubmit)}>
+        {errors.name && errors.name.type === 'required' && (
+          <span role="alert">Name Required</span>
+        )}
         <input
           className="w-30 border-green-600 border-2"
           type="text"
-          name="user_name"
+          name="name"
+          aria-invalid={errors.name ? 'true' : 'false'}
+          {...register('name', { required: true, maxLength: 40 })}
           placeholder="Name"
         />
 
+        {errors.email && errors.email.type === 'required' && (
+          <span role="alert">Email Required</span>
+        )}
         <input
           className="w-30 border-red-600 border-2"
           type="email"
-          name="user_email"
+          name="email"
+          {...register('email', { required: true, maxLength: 40 })}
           placeholder="Email"
         />
 
+        {errors.message && errors.message.type === 'required' && (
+          <span role="alert">Message Required</span>
+        )}
         <textarea
           className="w-80 h-40 border-blue-600 border-2"
+          {...register('message', { required: true, maxLength: 2500 })}
           name="message"
           id=""
           cols="30"
           rows="10"
-        >
-          Message...
-        </textarea>
+        />
+
+        <p className>{messageCharsLeft}</p>
 
         <input
           className="w-20 h-30 border-blue-600 border-2"
           type="submit"
           value="Send"
+        />
+        <input
+          className="w-20 h-30 border-blue-600 border-2"
+          type="reset"
+          value="Clear"
         />
       </form>
     </div>
