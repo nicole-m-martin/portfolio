3061a69d7aea9d412bcaaa0fd184481bd706e3ff
Nicole Martin <n.martinpdx@gmail.com>
2022-02-07 21:13:57 -0800

    refactor styles on Projects component

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -5,17 +5,15 @@ import Windows from '../../assets/coding.png';
 
 const Projects = () => {
   return (
-    <div className="bg-white dark:bg-gray-600">
-      <section class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12 bg-white dark:bg-gray-600">
-        <div class="text-center pb-12">
-          <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900 dark:text-white font-Poppins">
-            Here are some of my projects:
-          </h1>
-          <div className="flex justify-center">
+    <div className={styles.backgroundDiv}>
+      <section className={styles.centerDiv}>
+        <div className={styles.textDiv}>
+          <h1 className={styles.projectTitle}>Here are some of my projects:</h1>
+          <div className={styles.imageCenter}>
             <img
               src={Windows}
               alt="comp windows"
-              className="object-center h-60"
+              className={styles.projectImage}
             />
           </div>
 
@@ -38,6 +36,17 @@ const Projects = () => {
       </section>
     </div>
   );
+};
+
+const styles = {
+  backgroundDiv: 'bg-white dark:bg-gray-600',
+  centerDiv:
+    'max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12 bg-white dark:bg-gray-600',
+  textDiv: 'text-center pb-12',
+  projectTitle:
+    'font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900 dark:text-white font-Poppins',
+  imageCenter: 'flex justify-center',
+  projectImage: 'object-center h-60',
 };
 
 export default Projects;
