24ac98fb6482707c3a6ffefa72f77688f48e6c5b
Nicole Martin <n.martinpdx@gmail.com>
2022-02-11 11:35:33 -0800

    add loading spinner

--- src/Components/Giphy/Giphy.js
+++ src/Components/Giphy/Giphy.js
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Spinner from '../UI/Spinner';
 
 const Giphy = () => {
   const [gifs, setGifs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
+      // setLoading(true);
+
       const results = await axios(
-        `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&limit=1`
+        `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&limit=25`
       );
       console.log(results);
       setGifs(results.data.data);
+
+      // setLoading(false);
     };
+
     fetchData();
   }, []);
 
   const renderGifs = () => {
+    // if (setLoading) {
+    //   return <Spinner />;
+    // }
+
     return gifs.map((gif) => {
       return (
         <div key={gif.id} className="gifs">
-          <img src={gif.images.fixed_height.url} alt="gif" />
+          <img src={gif.images.downsized_medium.url} alt="gif" />
         </div>
       );
     });
