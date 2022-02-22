e90b1239c44c549537510f698aaeed6be5fca2fb
Nicole Martin <n.martinpdx@gmail.com>
2022-02-21 19:13:16 -0800

    update resume redirect to 2022 version

--- src/Components/Giphy/Gifs.js
+++ src/Components/Giphy/Gifs.js
@@ -1,11 +1,11 @@
-import React from 'react';
+// import React from 'react';
 
-const Gifs = ({ id, img }) => {
-  return (
-    <div key={id} className="gifs">
-      <img src={img} alt="gif" />
-    </div>
-  );
-};
+// const Gifs = ({ id, img }) => {
+//   return (
+//     <div key={id} className="gifs">
+//       <img src={img} alt="gif" />
+//     </div>
+//   );
+// };
 
-export default Gifs;
+// export default Gifs;
